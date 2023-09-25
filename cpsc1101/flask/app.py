91929c79e0974e9d029faaee091c34a3a4196fe8
Elucid <paulujlaky@gmail.com>
2023-09-25 15:04:33 -0400

    Add htmx components
    Co-authored-by: joshua-orndorff <joshua-orndorff@users.noreply.github.com>

--- cpsc1101/flask/app.py
+++ cpsc1101/flask/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+# Simple web server to store logged in data in memory using web components with HTMX on frontend
+
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,5 +9,65 @@ CORS(app)
 @app.route('/login', methods=['POST'])
 def login():
 
-    print("Login Route Requested!")
-    return "Login Route"
+    # Get the username and password from the request body
+    Username = request.form.get('username')
+    Password = request.form.get('password')
+
+    if (not Username or not Password):
+        return LoginElement("Missing username or password", False).ToHTML()
+
+    # Create a new user from the username and password
+
+    CreatedUser = user(Username, Password)
+
+    return LoggedInElement("Logged in successfully", True, CreatedUser).ToHTML()
+
+# Since we're not using files, if program restarts, all data is lost 
+
+class user: 
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
+        self.data = { }
+    
+    def AddData(self, key, value):
+        self.data[key] = value
+
+class LoginElement:
+
+    def __init__(self, message, success):
+        self.message = message
+        self.success = success
+
+    def ToHTML(self): 
+        return f"""            
+        <label for="username">Username</label>
+            <input type="text" name="username" id="username"/> 
+        <br/>
+        <br>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password"/> 
+        <br/>
+        <br>
+
+        {self.success and f"<p>{self.message}</p>" or f"<p style='color: red;'>{self.message}</p>"}
+
+        <input type="submit" name="submit" id="submit" value="Login"/> 
+        """
+
+class LoggedInElement:
+
+    def __init__(self, message, success, user):
+        self.message = message
+        self.success = success
+        self.user = user
+
+    def ToHTML(self): 
+        return f"""            
+        <div class="LoggedInElement"/>
+            <p>Logged in successfully</p>
+            <p>Username: {self.user.username}</p>
+            <p>Password: {self.user.password}</p>
+        """
