11caf979ece8e29dcf972aebb31c8ddaaf4bdc63
Elucid <paulujlaky@gmail.com>
2023-09-25 14:37:52 -0400

    Use htmx and flask cors

--- cpsc1101/flask/app.py
+++ cpsc1101/flask/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello_world():
+@app.route('/login', methods=['POST'])
+def login():
 
-    return 'Hello, World!'
+    print("Login Route Requested!")
+    return "Login Route"
