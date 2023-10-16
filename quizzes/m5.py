e089114dc2ead19a9bf7c20ecbca28571b566039
Elucid <paulujlaky@gmail.com>
2023-10-16 14:13:48 -0400

    M5 quiz

---
+++ quizzes/m5.py
@@ -0,0 +1,24 @@
+# # Question 1
+
+# for i in range(1000):
+#     if (i % 13 == 0):
+#         print(i)
+
+# # Question 2
+
+# i = 0
+
+# while i < 1000:
+#     if (i % 13 == 0):
+#         print(i)
+#     i += 1
+
+# # Question 3
+
+# arr = [5,3,7,5,3,9,7,8,3,5,6,11,7,13,4,17]
+
+# for element in arr:
+#     if (element % 2 == 0):
+#         print(f"{element} is even.")
+#     else:
+#         print(f"{element} is odd.")
