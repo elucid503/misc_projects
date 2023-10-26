44ce2d257f3388736a7cbd1897560d987425cd52
Elucid <paulujlaky@gmail.com>
2023-10-26 14:15:56 -0400

    Midterm review

---
+++ misc/class/review.py
@@ -0,0 +1,50 @@
+from random import randint
+
+# Exercise 1
+
+name_list = ["Andrea", "Patrick", "Aaron", "David", "Lynn", "Oliver", "Grace", "Roman"]
+for name in name_list:
+    print("Hello, " + name + "!")
+
+# Exercise 2
+
+set_str = "You must cut down the mightiest tree in the forest with…a herring!"
+
+count = 0
+while count < len(set_str):
+    count += 1
+
+print(count)
+
+# Exercise 3
+
+num_list = [20, -3, -2, 42, 1, -66, 8, 30, -4, 76, 33, -16]
+new_list = []
+
+for num in num_list:
+
+    if num > 0:
+        new_list.append(num)
+
+print(new_list)
+
+# Exercise 4
+
+d = {"car": "ford", "age": 21, 42: "everything", "result": True, "animals": ["dog", "cat", "rat"], "negative": -10}
+
+for key in d:
+    print(type(d[key]))
+
+# Exercise 5
+
+d_1 = {"positive": "NA", "positive": "NA", "negative": "NA", "positive": "NA", "negative": "NA", "negative": "NA"}
+
+for key in d_1:
+
+    if (key == "positive"): 
+        d_1[key] = randint(1, 100)
+
+    else:
+        d_1[key] = randint(-100, -1)
+
+print(d_1)
