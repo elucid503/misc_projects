cf64da7622ccfba1a199b41c92de173e3259ea56
Elucid <55906174+elucid503@users.noreply.github.com>
2023-11-20 10:20:35 -0500

    Create movies.py

---
+++ misc/class/movies.py
@@ -0,0 +1,47 @@
+def ListMovies(movies):
+    if movies:
+        print("\nList of Movies:")
+        for title, year in movies.items():
+            print(f"- {title} ({year})")
+    else:
+        print("\nNo movies in the list.")
+
+def AddMovie(movies):
+    title = input("Enter the movie title: ")
+    year = input("Enter the movie year: ")
+    movies[title] = year
+    print(f"{title} added to the list.")
+
+def DeleteMovie(movies):
+    title = input("Enter the movie title to delete: ")
+    if title in movies:
+        del movies[title]
+        print(f"{title} has been deleted.")
+    else:
+        print("Movie not found.")
+
+def main():
+    movies = {}
+
+    while True:
+        print("\nMovie List Program")
+        print("1. List all movies")
+        print("2. Add a movie")
+        print("3. Delete a movie")
+        print("4. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            ListMovies(movies)
+        elif choice == '2':
+            AddMovie(movies)
+        elif choice == '3':
+            DeleteMovie(movies)
+        elif choice == '4':
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid choice. Please choose a valid option.")
+
+if __name__ == "__main__":
+    main()
