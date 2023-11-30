778fa76d0aabe4478b245b5dc536c93be182d69b
Elucid <paulujlaky@gmail.com>
2023-11-30 15:07:21 -0500

    Add some general files

--- misc/class/files.py
+++ misc/class/files.py
@@ -1,3 +1,5 @@
+import csv
+
 # file = open('./test.txt', 'w')
 
 # file.write('Hello, world!')
@@ -6,3 +8,9 @@
 
 with open('./test.txt', 'a') as file:
     file.write('\nHello, again!')
+
+movies = [["Where the Crawdads Sing", 2022],["The Matrix", 1999],["The Shawshank Redemption", 1994], ["Die Hard", 1988]]
+
+with open('./movies.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(movies)
