327a9fe044bebc2b4a5f7392b9c54fdcecaf6146
Elucid <paulujlaky@gmail.com>
2023-11-30 15:08:36 -0500

    Refactor movie list format for better readability

--- misc/class/files.py
+++ misc/class/files.py
@@ -9,7 +9,8 @@ import csv
 with open('./test.txt', 'a') as file:
     file.write('\nHello, again!')
 
-movies = [["Where the Crawdads Sing", 2022],["The Matrix", 1999],["The Shawshank Redemption", 1994], ["Die Hard", 1988]]
+movies = [["Where the Crawdads Sing", 2022], ["The Matrix", 1999], ["The Shawshank Redemption", 1994],
+          ["Die Hard", 1988]]
 
 with open('./movies.csv', 'w', newline='') as file:
     writer = csv.writer(file)
