b91a46384b37d765ff5abf5a9800598c965154af
Elucid <paulujlaky@gmail.com>
2023-12-11 15:15:47 -0500

    Fix track name comparison in Spotify.py

--- misc/class/spotify.py
+++ misc/class/spotify.py
@@ -7,9 +7,8 @@ def main():
     with open(path, "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            if row["track_name"] == "":
-                print(f"Track: {row['track_name']}, Artist: {row['artist_name']}, Plays: {row['streams']}")
+            if (row["track_name"]).lower() == "con la brisa":
+                print(f"Track: {row['track_name']}, Artist: {row['artist(s)_name']}, Plays: {row['streams']}")
             
-
 if __name__ == "__main__":
     main()
